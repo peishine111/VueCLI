be52c5b4aaeecf21f739676e3e9157245f1c0b1c
pei.shine111 <pei.shine111@gmail.com>
2022-02-24 16:01:50 +0800

    VueCLI全部

---
+++ src/views/Home.vue
@@ -0,0 +1,58 @@
+<template>
+
+ <div class="home">
+
+  <Form v-slot="{ errors, values, validate }" @submit="onSubmit">
+
+   {{ errors }} {{ values }}
+
+   <div class="mb-3">
+
+    <label for="email" class="form-label">Email</label>
+
+    <Field id="email" name="email" type="email"
+
+     class="form-control" :class="{ 'is-invalid': errors['email'] }"
+
+     placeholder="請輸入 Email" rules="email|required" v-model="user.email"></Field>
+
+    <error-message name="email" class="invalid-feedback"></error-message>
+
+   </div>
+
+   <button class="btn me-2 btn-outline-primary" type="button" @click="validate">驗證</button>
+
+   <button class="btn btn-primary" type="submit">Submit</button>
+
+  </Form>
+
+ </div>
+
+</template>
+
+<script>
+
+export default {
+
+  name: 'Home',
+
+  data () {
+    return {
+
+      user: {}
+
+    }
+  },
+  methods: {
+
+    onSubmit () {
+      console.log(this.user)
+    }
+  },
+  created () {
+    console.log(this)
+  }
+
+}
+
+</script>
